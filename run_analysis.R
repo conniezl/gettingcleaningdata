b35bebb1a1e1efac1e1514d65d00b857b6948d8b
conniezl <connie_zarate@rogers.com>
2015-01-25 04:04:49 -0500

    script version 2

--- run_analysis.R
+++ run_analysis.R
@@ -1,9 +1,11 @@
 # Requisites:
 # The script run_analysis.R must reside in the working directory
-# The "UCI HAR" folder must reside in the working directory, with all the data set files as provided in the .zip file (./UCI HAR/*) 
+# The "UCI HAR" folder must reside in the working directory, with all the data set files as provided in 
+#     the .zip file (./UCI HAR/*). 
 # the package "reshape2" must be installed.
 
-# Output: The function writes the resulting tidy data set in the file "./tidyDataSet.txt", and returns the tidy data set as a data frame.
+# Output: The function writes the resulting tidy data set in the file "./tidyDataSet.txt", and returns the tidy data set 
+#         as a data frame.
 
 # Usage: 
 
@@ -11,18 +13,21 @@
 # source("run_analysis.R")
 # tidyDataset <- run_analysis()
 
-# The 9 signals files in the ./test/Inertial Signals and ./training/Inertial Signals folders were not used as they are not required    
+# The 9 signals files in the ./test/Inertial Signals and ./training/Inertial Signals folders were not used as they are 
+# not required.
 
 run_analysis <- function () {  
   
   # 1. Prepare Activities data set for Training and Test. 
   #
   #   - Read activity labels list
-  #   - Get the sequence of activities observed for test and training. Each record represents the activity for one sliding window. 
+  #   - Get the sequence of activities observed for test and training. Each record represents the activity for one 
+  #     sliding window. 
   #   - Append training and test rows together using rbind
   #   - Add a new column in the appended data set, that has the "label" of the activity (i.e. WALKING, SITTING, etc.).
   #       (i.e. for this, merge the appended dataset with the Activity labels dataset)
-  #       Note: the merge function changes the order of the records. To remember the order, the row.names were added as a column before merging.
+  #       Note: the merge function changes the order of the records. To remember the order, the row.names were added as 
+  #       a column before merging.
   #             In this data set the row.names have the row number, so they can be used to re-sort when needed.
   
   activityLabels <- read.table("./UCI HAR Dataset/activity_labels.txt") 
@@ -32,13 +37,16 @@ run_analysis <- function () {
   activities <- rbind(trainActivities,testActivities)
   
   
-  activities<-data.frame(activities, rn= as.numeric(row.names(activities)))                   # Add a new column with the row.names to 
-                                                                                              # remember the original order of the records. 
+  activities<-data.frame(activities, rn= as.numeric(row.names(activities))) # Add a new column with the row.names to 
+                                                                            # remember the original order of the records. 
   
-  labeledActivities <- merge(activityLabels, activities, by = "V1", all.y=TRUE, sort=FALSE)   # merge activities with activity labels 
-  labeledActivities <- labeledActivities[order(labeledActivities$rn),]                        # resort records based on the original row.names saved before
+  labeledActivities <- merge(activityLabels, activities, by = "V1", all.y=TRUE, sort=FALSE)   # merge activities with 
+                                                                                              # activity labels 
+  labeledActivities <- labeledActivities[order(labeledActivities$rn),]                        # resort records based on the 
+                                                                                              # original row.names saved before
   
-  rownames(labeledActivities) <- labeledActivities$rn                                         # reassign new row.names now that order is correct
+  rownames(labeledActivities) <- labeledActivities$rn                                         # reassign new row.names now that 
+                                                                                              # order correct
   colnames(labeledActivities)[1]<- "activity"                                                 # reassign new descriptive variable names 
   colnames(labeledActivities)[2]<- "activityType"
   
@@ -61,7 +69,8 @@ run_analysis <- function () {
   #   - Read features list and extract only mean() and std() type of measurements.
   #   - Read the features vector for each sliding window, for both test and training.
   #   - Append training and test rows together using rbind
-  #   - Reduce the featuresVector in the appended data set to have only the mean() and std() measurements. Reduced from 561 to 66 measurements. 
+  #   - Reduce the featuresVector in the appended data set to have only the mean() and std() measurements. 
+  #     Reduced from 561 to 66 measurements. 
   #           Note: this step is done before merging to reduce the processing time.
   #   - Assign descriptive names to variables
   
@@ -116,9 +125,10 @@ run_analysis <- function () {
   
   # 6. FINAL STEP: Create a Tidy Data Set with the average of each variable for each activity and each subject.
   #   - reshape (melt) the dataset from step 5 to have the measurements in rows instead of columns. 
-  #     Tne new dataset would have columns: ActivityType, subject, variable, and value, where the variable and value columns will have 
-  #     each of the measurements and its value for each ACtivity type and subject. This creates a narrow, tall data set instead. 
-  #   - dcast the resulting dataset to put back the measurements as columns and calculate at the ame time the mean by activityType, subject, and measurement
+  #     Tne new dataset would have columns: ActivityType, subject, variable, and value, where the variable and value columns 
+  #     will have each of the measurements and its value . This creates a narrow, tall data set instead. 
+  #   - dcast the resulting dataset to put back the measurements as columns and calculate at the ame time the mean by 
+  #     activityType, subject, and measurement
   
   require (reshape2)
 
